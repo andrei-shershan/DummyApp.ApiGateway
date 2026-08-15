f56d6c08cba99755a788c0df3552024513743092
Andrei Shershan <mail.shershan@gmail.com>
2026-08-16 00:00:12 +0200

    Attach PDF to email

--- src/DummyApp.ApiGateway.Infrastructure/HttpClients/IFileServiceHttpClient.cs
+++ src/DummyApp.ApiGateway.Infrastructure/HttpClients/IFileServiceHttpClient.cs
@@ -3,4 +3,5 @@ namespace DummyApp.ApiGateway.Infrastructure.HttpClients;
 public interface IFileServiceHttpClient
 {
     Task<string> GenerateQrCodeBase64Async(string text, int pixelsPerModule, CancellationToken cancellationToken);
+    Task<byte[]> GeneratePdfAsync(GeneratePdfRequest request, CancellationToken cancellationToken);
 }
